7cb2b3f7bc8649c9f01738838e9f44642188e3d2
Juan Damian Rojas <damianxrojas@gmail.com>
2023-04-30 22:44:53 -0400

    added a little bit of testing to react components

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import UserInputForm from './components/UserInputForm';
 
-test('renders learn react link', () => {
+test('renders initial load for fizzbuzz app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Leaderboard');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders form and expects button to be disabled on initial render.', () => {
+  render(<UserInputForm />)
+  const button = screen.getByText('Submit')
+  expect(button).toBeDisabled();
+});
+
+test('renders form and expects button to not be disabled after being typed.', () => {
+  render(<UserInputForm />)
+  const button = screen.getByText('Submit')
+
+  const label = screen.getByLabelText('Input:')
+  fireEvent.change(label, {target: {value: "10"}})
+  expect(button).not.toBeDisabled();
 });
